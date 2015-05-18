1708563068c45166a9067309a62dc92d5063e56c
Oguz Bayraktar <fayt@cuthalion.com>
2015-05-18 20:01:42 +0300

    waterdeep db fix

--- EStore/wsgi.py
+++ EStore/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import site
+
+site.addsitedir('site.addsitedir('/home/fayt/.local/lib/python2.7/site-packages/')')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EStore.settings")
 
 from django.core.wsgi import get_wsgi_application
